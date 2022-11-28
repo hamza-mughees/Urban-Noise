20902e33e2451a82bd378e29cbf45d003a9a50e7
hamza-mughees <mugheesh@tcd.ie>
2022-11-28 23:46:00 +0000

    Fixed promise error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
-import { getAllLeaq } from './utils/apiReqs.js'
+import React, { useEffect, useState } from "react";
+import { getAllLaeq } from "./utils/apiReqs.js";
 // import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const App = () => {
+  const [laeq, setLaeq] = useState({})
+
   useEffect(() => {
-    getAllLeaq().then((res) => {
-      console.log(res)                        // don't know what the hell is going on here
-      console.log(Object.keys(res).length)
-      console.log(Object.keys(res))
-      console.log(res['10.1.1.1'])
-    })
+    (async () => {
+      const res = await getAllLaeq()
+      setLaeq(res)
+    })()
   }, [])
 
-  return (<div className="App">Hello world</div>)
+  return <div className="App">Hello world</div>;
 
   // return (
   //   <ResponsiveContainer width="100%" height="100%">
@@ -23,6 +23,6 @@ const App = () => {
   //     ></LineChart>
   //   </ResponsiveContainer>
   // )
-}
+};
 
 export default App;
--- src/utils/apiReqs.js
+++ src/utils/apiReqs.js
@@ -1,65 +1,60 @@
-var headers = new Headers()
-headers.append('Content-Type', 'application/json')
+var headers = new Headers();
+headers.append("Content-Type", "application/json");
 
 var requestOptions = {
-  method: 'POST',
+  method: "POST",
   headers: headers,
-  redirect: 'follow'
-}
+  redirect: "follow",
+};
 
 var body = {
-  username: 'dublincityapi',
-  password: 'Xpa5vAQ9ki'
-}
+  username: "dublincityapi",
+  password: "Xpa5vAQ9ki",
+};
 
-export const getMonitors = async () => {
-  return await (await fetch('https://data.smartdublin.ie/sonitus-api/api/monitors', {
-    ...requestOptions,
-    body: JSON.stringify(body)
-  })).json()
-}
-
-export const getMonitorData = async (serial_number) => {
-  return await (await fetch('https://data.smartdublin.ie/sonitus-api/api/data', {
-    ...requestOptions,
-    body: JSON.stringify({
-      ...body,
-      monitor: serial_number,
-      start: '1668800000',
-      end: '1669331670'
+export const getMonitors = async () =>
+  (
+    await fetch("https://data.smartdublin.ie/sonitus-api/api/monitors", {
+      ...requestOptions,
+      body: JSON.stringify(body),
     })
-  })).json()
-}
+  ).json();
 
-export const getAllLeaq = async () => {
-  var monitors = await getMonitors()
-  var allLaeq = {}
+export const getMonitorData = async (serial_number) =>
+  (
+    await fetch("https://data.smartdublin.ie/sonitus-api/api/data", {
+      ...requestOptions,
+      body: JSON.stringify({
+        ...body,
+        monitor: serial_number,
+        start: "1668800000",
+        end: "1669331670",
+      }),
+    })
+  ).json();
 
-  // console.log(monitors)
+export const getAllLaeq = async () => {
+  var monitors = await getMonitors();
+  var allLaeq = {};
 
-  for (const i in monitors)  {  
-    const m = monitors[i] 
-    if (m.label.split(' ')[0] === 'Noise' && m.location !== "In Office") {
-      // console.log(`Interation ${i}`, m)
-      
-      getMonitorData(m.serial_number).then((monitorData) => {
-        var monitorLaeq = []
-
-        monitorData.forEach((r) => {
-          monitorLaeq.push({
-            datetime: r.datetime,
-            laeq: r.laeq,
-          })
-        })
-        
-        allLaeq[m.serial_number] = {
-          label: m.label,
-          laeq: monitorLaeq
-        }
-      })
-      // console.log(allLaeq)
+  const promises = monitors.map(async (m) => {
+    if (m.label.split(" ")[0] !== "Noise" || m.location === "In Office") {
+      return
     }
-  }
+    
+    await getMonitorData(m.serial_number).then((monitorData) => {
+      var monitorLaeq = monitorData.map((r) => ({
+        datetime: r.datetime,
+        laeq: r.laeq,
+      }));
 
-  return allLaeq
-}
+      allLaeq[m.serial_number] = {
+        label: m.label,
+        laeq: monitorLaeq,
+      };
+    });
+  });
+  await Promise.all(promises);
+
+  return allLaeq;
+};
